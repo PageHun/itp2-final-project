88555e428b0baa9896efd1219ab58f5b0e7e0945
kulann19 <kulanalnur@gmail.com>
2026-05-18 22:12:05 +0000

    Add unit test setup for FinanceTracker

--- test_tracker.py
+++ test_tracker.py
@@ -2,3 +2,9 @@ import unittest
 import os
 from models import User, IncomeTransaction, ExpenseTransaction
 from tracker import FinanceTracker
+
+class TestFinanceTracker(unittest.TestCase):
+    def setUp(self):
+        self.test_file = "test_data.json"
+        self.user = User("TestUser")
+        self.tracker = FinanceTracker(self.user, file_path=self.test_file)
