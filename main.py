b68f9212d00b9ebce10cf072745c444ad4c26209
PageHun <107790070+PageHun@users.noreply.github.com>
2026-05-20 09:26:58 +0500

    Implement transaction additions in main function
    Add transactions for income and expenses in main function

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def main():
     print("Инициализация системы Personal Finance Tracker")
     user = PremiumUser(username="Sanzhar", extra_limit=1500.0)
     tracker = FinanceTracker(user=user, file_path="data.json")
+
+    try:
+        tracker.add_transaction(IncomeTransaction(amount=2000.0, date="2026-05-01"))
+        tracker.add_transaction(ExpenseTransaction(amount=400.0, date="2026-05-01", category="food"))
+        tracker.add_transaction(ExpenseTransaction(amount=1200.0, date="2026-05-02", category="transport"))
+        tracker.add_transaction(ExpenseTransaction(amount=200.0, date="2026-05-03", category="food"))
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+
+    tracker.save_to_file()
