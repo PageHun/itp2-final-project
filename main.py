9d63f252b4bf7d62c21150c7fcf946885c656a52
PageHun <107790070+PageHun@users.noreply.github.com>
2026-05-20 09:19:49 +0500

    Initialize Personal Finance Tracker system

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from models import PremiumUser, IncomeTransaction, ExpenseTransaction
 from tracker import FinanceTracker
+
+def main():
+    print("Инициализация системы Personal Finance Tracker")
+    user = PremiumUser(username="Sanzhar", extra_limit=1500.0)
+    tracker = FinanceTracker(user=user, file_path="data.json")
